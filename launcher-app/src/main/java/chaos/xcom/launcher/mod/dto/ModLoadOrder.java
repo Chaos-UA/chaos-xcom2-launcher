4b4885c38df04c0f001e4f6af9d13b103baaed51
Chaos <UkraineChaos@gmail.com>
2025-12-17 17:35:23 +0200

    1

--- launcher-app/src/main/java/chaos/xcom/launcher/mod/dto/ModLoadOrder.java
+++ launcher-app/src/main/java/chaos/xcom/launcher/mod/dto/ModLoadOrder.java
@@ -2,15 +2,15 @@ package chaos.xcom.launcher.mod.dto;
 
 public enum ModLoadOrder {
     BEFORE,
-    LOAD_AFTER_REQUIRED,
-    LOAD_AFTER_REQUIRED_REPLACEMENT,
+    AFTER_REQUIRED,
+    AFTER_REQUIRED_REPLACEMENT,
     AFTER;
 
     /**
      * this one has less priority than LOAD_BEFORE/LOAD_AFTER
      */
     public boolean isLoadAfterRequired() {
-        return  this == LOAD_AFTER_REQUIRED || this == LOAD_AFTER_REQUIRED_REPLACEMENT;
+        return  this == AFTER_REQUIRED || this == AFTER_REQUIRED_REPLACEMENT;
     }
 
     public boolean isLoadBefore() {
