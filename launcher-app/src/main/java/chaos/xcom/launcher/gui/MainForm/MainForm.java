a1d8f0ea5215debd0fdfeae97aeb89bd7b44ed69
Chaos <UkraineChaos@gmail.com>
2025-12-20 13:25:06 +0200

    1

--- launcher-app/src/main/java/chaos/xcom/launcher/gui/MainForm/MainForm.java
+++ launcher-app/src/main/java/chaos/xcom/launcher/gui/MainForm/MainForm.java
@@ -433,7 +433,7 @@ public class MainForm extends JFrame {
         lblErrorsDetected.setHorizontalAlignment(2);
         lblErrorsDetected.setHorizontalTextPosition(2);
         lblErrorsDetected.setText("");
-        lblErrorsDetected.setToolTipText("Some active mods have errors");
+        lblErrorsDetected.setToolTipText("Some active mods have errors and might work not properly.\nManual resolve needed.");
         lblErrorsDetected.setVisible(false);
         panel10.add(lblErrorsDetected, new GridConstraints(0, 1, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
         final Spacer spacer1 = new Spacer();
