52e8bbf6af2dc2a73ac87bd2aaac42217ac3aead
Chaos <UkraineChaos@gmail.com>
2026-06-13 15:44:13 +0300

    Added steam integration

--- launcher-app/src/main/java/chaos/xcom/launcher/gui/SettingsDialog/SettingsDialog.java
+++ launcher-app/src/main/java/chaos/xcom/launcher/gui/SettingsDialog/SettingsDialog.java
@@ -535,11 +535,11 @@ public class SettingsDialog extends JDialog {
         panel8.setLayout(new GridLayoutManager(1, 2, new Insets(0, 0, 0, 0), -1, -1));
         panel6.add(panel8, new GridConstraints(3, 0, 1, 3, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
         steamRequestDelaySpinner = new FlatSpinner();
-        steamRequestDelaySpinner.setToolTipText("Steam may return 429 (too many requests) error if requests done too often");
+        steamRequestDelaySpinner.setToolTipText("Used only as fallback when Steam for some reason is not running \nto parse mods info and required mods from HTML page and is very slow.\nSteam may return 429 (too many requests) error if requests done too often via fallback.");
         panel8.add(steamRequestDelaySpinner, new GridConstraints(0, 1, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
         final JLabel label8 = new JLabel();
         label8.setText("Steam mod sync request delay in seconds");
-        label8.setToolTipText("Steam may return 429 (too many requests) error if requests done too often");
+        label8.setToolTipText("Used only as fallback when Steam for some reason is not running \nto parse mods info and required mods from HTML page and is very slow.\nSteam may return 429 (too many requests) error if requests done too often via fallback.");
         panel8.add(label8, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
         final Spacer spacer3 = new Spacer();
         contentPane.add(spacer3, new GridConstraints(4, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_VERTICAL, 1, GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
